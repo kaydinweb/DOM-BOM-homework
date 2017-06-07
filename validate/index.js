8224b309838a843ddf18340fabedd12b94d34955
Orest Kaydrovych <o.kaydrovych77@gmail.com>
2017-06-07 19:47:34 +0300

    Fix age form validation, now it valid just a numbers, in index.js

--- validate/index.js
+++ validate/index.js
@@ -30,33 +30,42 @@ var submit = document.createElement('input');
 	submit.setAttribute("value", "VALIDATE");
 	form.appendChild(submit);
 
-
 function checkName(){
 	var name = document.getElementsByTagName('input')[0].value;
 	var x = name.match(/^user_[^;:#*%S]/i);
 	if (!x){
 		alert("Your name must begin with user_")
-	} return true;
+	} return false;
 }
-
 
 function checkAge(){
 	var age = document.getElementsByTagName("input")[1].value;
-	var x = age.match(/^[0-9]/);
+	var x = age;
 	if(!x){
 		alert("Please type only numbers")
-	} return true;
+		return false;
+	}if (x == ""){
+	alert("Please type your age!!");
+	return false;
+	} else 
+	if (isNaN(x)||x<0||x>Infinity){
+	alert("Please type just a numbers!!");
+	return false;
+	}else
+	if (x == 0 || x < 0) {
+	alert("Numbers can't be negative or equal zero!!")
+	return false;
+	}
 }
-
 
 function checkData(){
 	var data = document.getElementsByTagName('input')[2].value;
 	var x = data.match(/^[0-3][0-9]\/[0|1][0-9]\/(19|20)[0-9]{2}/);
 	if(!x){
 		alert('Please type date like this dd/mm/yyyy')
-	}return true;
+		return false;
+	}
 }
-
 
 function checkForm(form){
 	checkAge();
